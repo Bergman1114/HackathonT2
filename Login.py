ba51238529b57b41435234a2c2c63057e19d28b8
Bryan Berg <81443115+Bergman1114@users.noreply.github.com>
2021-04-29 13:54:50 -0400

    what_i_have
    what i got, not really sure what im doing like everyone else

--- Login.py
+++ Login.py
@@ -7,11 +7,23 @@ username = 'Kite'
 password = 'pass'
 path = './HackathonT2/logs.txt'
 
-user = input('What is your username? >> ')
-passwd = input('What is your password? >> ')
+user = input('Please enter your username : ')
+passwd = input('Please enter your password : ')
 
-def printSuccessOrNot(message):
-    if (message == 'You'):
+def logopt():
+    print("Which would you like to access >> \n 1. Exsiting Customer \n 2. New Customer \n 3. Exsisting Seller \n 4. New Seller")
+    choice = input()
+    if choice == "1":
+        UserLogYN()
+    if chocie == "2":
+        logopt()
+    if chocie == "3":
+        logopt()
+    if chocie == "4":
+        logopt()
+
+def UserLogYN(message):
+    if (message == 'Welcome'):
         with open(path, 'a') as log:
             log.write(f'\nUser {user} tried to log in on {datetime.now()} - Result: {message}')
             log.close()
@@ -21,10 +33,8 @@ def printSuccessOrNot(message):
             log.close()
 
 if(user == username and passwd == password):
-    print(f'Hello {user}, you are fully authenticated')
-    printSuccessOrNot('Success')
+    print(f'Hello {user}, you are logged in and fully authenticated')
+    UserLogYN('Welcome')
 else:
-    print('The combination of the username and password you used does not match our records')
-    printSuccessOrNot('Failed')
-
-    # testing for push
+    print('The username and password you used does not match our records')
+    UserLogYN('Denied')
